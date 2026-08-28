147c85689aae56020481f568b0431a45460e77ba
xzk410-stack <xzk410@gmail.com>
2026-08-28 18:18:29 +0800

    Publish Travelmate service worker

---
+++ travelmate/app/sw.js
@@ -0,0 +1,5 @@
+const CACHE='travelmate-web-formal-20260828';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./payload/part01.txt','./payload/part02.txt','./payload/part03.txt','./payload/part04.txt','./payload/part05.txt','./payload/part06.txt'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
