8176d98b4347a3a80a2090caf05ba74b5ef0e906
xzk410-stack <xzk410@gmail.com>
2026-09-09 09:17:35 +0800

    Finalize 我的消費簿 Android v1.0.4

--- android/spendbook/app/build.gradle.kts
+++ android/spendbook/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.xzk410.spendbook"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
     }
 
     if (
