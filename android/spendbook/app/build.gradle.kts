4b3f769ef5ce83e3bc7d6f17fad9f9bc8e2ab306
xzk410-stack <xzk410@gmail.com>
2026-09-09 08:54:36 +0800

    Bump 我的消費簿 Android to v1.0.3

--- android/spendbook/app/build.gradle.kts
+++ android/spendbook/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.xzk410.spendbook"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
 
     if (
