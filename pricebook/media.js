91a0d93fd9818c0ed95f87aa65b82bb7f871d512
xzk410-stack <xzk410@gmail.com>
2026-09-14 03:19:15 +0800

    Load 比價簿 freeze fix immediately

--- pricebook/media.js
+++ pricebook/media.js
@@ -112,7 +112,7 @@
       if(!doc.getElementById('pricebook-ui-polish-patch')){
         const script=doc.createElement('script');
         script.id='pricebook-ui-polish-patch';
-        script.src='./ui-polish-patch.js?v=20260914-ui1';
+        script.src='./ui-polish-patch.js?v=20260914-ui2';
         doc.body.appendChild(script);
       }
     }catch(e){}
