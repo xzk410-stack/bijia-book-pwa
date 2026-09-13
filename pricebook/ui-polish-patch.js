a6ca80cd9205f03611513caaea928c5ea5daeebd
xzk410-stack <xzk410@gmail.com>
2026-09-14 03:43:24 +0800

    Load quick new product metadata before smart suggestions

--- pricebook/ui-polish-patch.js
+++ pricebook/ui-polish-patch.js
@@ -14,5 +14,6 @@
   add('pricebook-final-quick-patch','./final-quick-patch.js?v=20260914-final3');
   add('pricebook-final-cleanup-patch','./final-cleanup-patch.js?v=20260914-final3');
   add('pricebook-quick-modal-hotfix','./quick-modal-hotfix.js?v=20260914-hotfix1');
-  add('pricebook-smart-fields-patch','./smart-fields-patch.js?v=20260914-smart1');
+  add('pricebook-quick-new-product-patch','./quick-new-product-patch.js?v=20260914-quickmeta1');
+  add('pricebook-smart-fields-patch','./smart-fields-patch.js?v=20260914-smart2');
 })();
