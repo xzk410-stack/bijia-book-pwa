07c769084c74a62936dafb81f027ecaa31c080fe
xzk410-stack <xzk410@gmail.com>
2026-09-15 02:09:55 +0800

    Bump pricebook cache after layout correction

--- pricebook/sw.js
+++ pricebook/sw.js
@@ -1,4 +1,4 @@
-const CACHE='pricebook-cloud-v3';
+const CACHE='pricebook-cloud-v4';
 const ASSETS=['./','./index.html','./app.html','./cloud.js','../manifest.webmanifest','../icons/icon-192.png','../icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('pricebook-')&&k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
